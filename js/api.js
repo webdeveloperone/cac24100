03e3317da4505a811c50de1528d45e72201cb8f8
webdeveloperone <168598807+webdeveloperone@users.noreply.github.com>
2024-06-02 19:49:00 -0300

    Upload Api, Script
    Upload Api, Script

--- js/api.js
+++ js/api.js
@@ -1,1 +1,25 @@
+let contenedorPersonajes = document.getElementsByTagName("personajes")
 
+fetch("https://rickandmortyapi.com/api/character")
+.then(response => response.json())
+
+.then(response => {
+ 
+	console.log(response)
+	console.log(response.results)
+
+	response.results.forEach((elementos) =>{
+	//console.log(elementos.name)
+
+	const contenedorCreado = document.createElement('div')
+
+  });
+
+ personajes.innerHTML= `
+	<h4>${elementos.name}</h4>
+
+ 	<img src="${elementos.image}">
+
+`;
+contenedorPersonajes.append(contenedorCreado) 	
+ });
