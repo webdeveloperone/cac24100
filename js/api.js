1145861d4177991b84ccf3c173df0cad4f63a4f9
webdeveloperone <168598807+webdeveloperone@users.noreply.github.com>
2024-06-03 00:16:54 -0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,12 +1,14 @@
 let contenedorPersonajes = document.getElementById('personajes')
 
 fetch("https://rickandmortyapi.com/api/character")
-//.then((response)=>response.json())
+.then((response)=>response.json())
 
-//.then((datos)=>{
+.then((datos)=>{
  
-//	console.log(datos)
-//	console.log(datos.results)
+	console.log(datos)
+	console.log(datos.results)
+
+})
 
 //	datos.results.forEach((elementos) => {
 //	console.log(elementos.name)
